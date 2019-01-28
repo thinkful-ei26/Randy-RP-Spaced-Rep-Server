8f9e65a01f132f04d72e404da11da69d6ffa95f9
RP Boyle <rpboyle11@yahoo.com>
2019-01-28 12:28:01 -0500

    added MongoDB in mlab

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+        process.env.DATABASE_URL || 'mongodb://randy:password123@ds115595.mlab.com:15595/italian-spaced-repetition',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test'
+        'mongodb://randy:password123@ds115595.mlab.com:15595/italian-spaced-repetition'
   // DATABASE_URL:
   //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
   // TEST_DATABASE_URL:

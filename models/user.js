089bf89b0ff2301356feeec3767e5040292a5a4a
RP Boyle <rpboyle11@yahoo.com>
2019-01-28 13:57:44 -0500

    added user schema start

---
+++ models/user.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, require: true},
+  // userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+});
+
+// Add `createdAt` and `updatedAt` fields
+userSchema.set('timestamps', true);
+
+// Transform output during `res.json(data)`, `console.log(data)` etc.
+userSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, result) => {
+    delete result._id;
+    delete result.__v;
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
